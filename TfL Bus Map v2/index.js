ddc345b6b177b2e327bbc7913395bd6688f895c1
Shiva Pingle <>
2024-03-04 18:53:20 +0000

    Update canvas code and data to show London boroughs

--- TfL Bus Map v2/index.js
+++ TfL Bus Map v2/index.js
@@ -1,15 +1,55 @@
 'use strict';
 
 const canvasOptions = {
-    width: 600,
-    height: 400
+    width: 540,
+    height: 420
 };
 
 const canvas = document.getElementById('tfl-bus-map');
-canvas.width = canvasOptions.width;
-canvas.height = canvasOptions.height;
-
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+// const dpr = window.devicePixelRatio ?? 1;
+const dpr = 4;
+canvas.width = canvasOptions.width * dpr;
+canvas.height = canvasOptions.height * dpr;
+
+// normalize coordinate system to use CSS pixels
+ctx.scale(dpr, dpr);
+
+canvas.style.width = canvasOptions.width + 'px';
+canvas.style.height = canvasOptions.height + 'px';
+
+// change to fetching from file
+const boundariesJSON = await fetch(
+    './data_files/london-borough-boundaries.json');
+let boroughData = await boundariesJSON.json();
+
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 0.6;
+
+ctx.translate(30, 30);
+
+for (const { WKT: linestring, Name } of boroughData) {
+    const lines = linestring.slice(11, -1)
+        .split(',')
+        .map(pair => pair
+            .split(' ')
+            .map(coord => +coord)
+        );
+
+    ctx.beginPath();
+    ctx.moveTo(...lines[0]);
+    lines.slice(1).forEach(coords => ctx.lineTo(...coords));
+    ctx.stroke();
+
+    // Name
+}
+
+ctx.resetTransform();
+
+
+// const link = document.createElement('a');
+// link.download = 'london-basemap.png';
+// link.href = canvas.toDataURL();
+// link.click();
